d501a6af3a6d33fa643d47c30b57b542506b2169
daykalif <1038200595@qq.com>
2025-02-25 16:27:17 +0800

    feat：MybatisPlus-08.核心功能-IService开发基础业务接口

---
+++ mp-demo/src/main/java/com/itheima/mp/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.itheima.mp.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.itheima.mp.domain.dto.UserFormDTO;
+import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.vo.UserVO;
+import com.itheima.mp.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "用户管理接口")    // swagger 注解
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor    // 仅针对 final 类型的属性生成对应的构造方法
+public class UserController {
+
+	private final IUserService userService;
+
+	@ApiOperation("新增用户接口")
+	@PostMapping
+	public void saveUser(@RequestBody UserFormDTO userDTO) {
+		// 1.把DTO拷贝到PO
+		User user = BeanUtil.copyProperties(userDTO, User.class);
+		// 2.新增
+		userService.save(user);
+	}
+
+
+	@ApiOperation("删除用户接口")
+	@DeleteMapping("{id}")
+	public void deleteUserById(@ApiParam("用户id") @PathVariable("id") Long id) {    // @ApiParam：swagger 注解
+		userService.removeById(id);
+	}
+
+
+	@ApiOperation("根据id查询用户接口")
+	@GetMapping("{id}")
+	public UserVO queryUserById(@ApiParam("用户id") @PathVariable("id") Long id) {
+		// 1.查询用户PO
+		User user = userService.getById(id);
+		// 2.把PO拷贝到VO
+		return BeanUtil.copyProperties(user, UserVO.class);
+	}
+
+
+	@ApiOperation("根据id批量查询用户接口")
+	@GetMapping
+	public List<UserVO> queryUserById(@ApiParam("用户id集合") @RequestParam("ids") List<Long> ids) {
+		// 1.查询用户PO
+		List<User> users = userService.listByIds(ids);
+		// 2.把PO拷贝到VO
+		return BeanUtil.copyToList(users, UserVO.class);
+	}
+}
